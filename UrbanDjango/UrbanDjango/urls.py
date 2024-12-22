888c5123e8451efe2ccb90033085d98a89a71fdd
valery-kiselev <voler@inbox.ru>
2024-12-22 19:08:30 +0400

    Additions
    Additions

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import func_template, class_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_template),
+    path('task/', class_template.as_view())
 ]
