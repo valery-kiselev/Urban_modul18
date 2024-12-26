f97d4420f6c2fdb8b6bfdcd08290383a1d762106
valery-kiselev <voler@inbox.ru>
2024-12-26 08:03:21 +0400

    Add task4

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'UrbanDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates/fourth_task'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def platform(request):
-    return render(request, 'platform.html')
+    return render(request, 'fourth_task/platform.html')
 
 def games(request):
     games = ["Atomic Heart", "Cyberpunk 2077"]
-    return render(request, 'games.html', {'games': games})
+    return render(request, 'fourth_task/games.html', {'games': games})
 
 def cart(request):
-    return render(request, 'cart.html')
+    return render(request, 'fourth_task/cart.html')
 
 
 
